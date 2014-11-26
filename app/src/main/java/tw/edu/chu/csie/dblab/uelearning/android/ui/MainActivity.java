ec101c5ad4c102cd18d1fb95ff96f7536306d496
Yuan Chiu <chyuaner@gmail.com>
2014-11-26 20:33:25 +0800

    帳號登出（並通知伺服端）

--- app/src/main/java/tw/edu/chu/csie/dblab/uelearning/android/ui/MainActivity.java
+++ app/src/main/java/tw/edu/chu/csie/dblab/uelearning/android/ui/MainActivity.java
@@ -5,9 +5,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.apache.http.protocol.HTTP;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import tw.edu.chu.csie.dblab.uelearning.android.R;
+import tw.edu.chu.csie.dblab.uelearning.android.config.Config;
 import tw.edu.chu.csie.dblab.uelearning.android.database.DBProvider;
+import tw.edu.chu.csie.dblab.uelearning.android.server.UElearningRestClient;
+import tw.edu.chu.csie.dblab.uelearning.android.util.ErrorUtils;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -38,14 +50,39 @@ public class MainActivity extends ActionBarActivity {
         }
         else if(id == R.id.menu_logout) {
 
-            // 清除登入資料
             DBProvider db = new DBProvider(MainActivity.this);
+
+            // 抓取目前已登入的Token
+            String token = db.get_token();
+
+            // 告訴伺服端說已登出
+            try {
+                UElearningRestClient.delete("/tokens/" + URLEncoder.encode(token, HTTP.UTF_8), new AsyncHttpResponseHandler() {
+                            @Override
+                            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                                if(Config.DEBUG_SHOW_MESSAGE) {
+                                    Toast.makeText(MainActivity.this, "伺服器已接受登出" ,Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                                Toast.makeText(MainActivity.this, "伺服器登出失敗" ,Toast.LENGTH_SHORT).show();
+                                ErrorUtils.error(MainActivity.this, error);
+                            }
+                });
+            } catch (UnsupportedEncodingException e) {
+                ErrorUtils.error(MainActivity.this, e);
+            }
+
+            // 清除登入資料
             db.remove_user();
 
             // 回到登入畫面
             finish();
             Intent to_login = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(to_login);
+
             return true;
         }
 
