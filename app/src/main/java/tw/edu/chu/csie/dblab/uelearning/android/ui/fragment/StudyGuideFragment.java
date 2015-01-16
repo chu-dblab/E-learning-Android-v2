fefab98258f0c1d78c4cfdb189cf40adb358f646
Yuan Chiu <chyuaner@gmail.com>
2015-01-16 21:28:23 +0800

    將StudyGuideFragment移出成獨立檔案

---
+++ app/src/main/java/tw/edu/chu/csie/dblab/uelearning/android/ui/fragment/StudyGuideFragment.java
@@ -0,0 +1,95 @@
+package tw.edu.chu.csie.dblab.uelearning.android.ui.fragment;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import tw.edu.chu.csie.dblab.uelearning.android.R;
+
+/**
+ * 學習引導畫面（顯示推薦學習點的地方）
+ */
+public class StudyGuideFragment  extends Fragment implements AdapterView.OnItemClickListener, SwipeRefreshLayout.OnRefreshListener {
+
+    private ListView mList_nextPoints;
+    private SwipeRefreshLayout mSwipe_nextPoints;
+    private TextView mText_remainedTime;
+    private ImageView mImage_map;
+
+    public static StudyGuideFragment newInstance(int sectionNumber) {
+        StudyGuideFragment fragment = new StudyGuideFragment();
+        return fragment;
+    }
+
+    public StudyGuideFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_study_guide, container, false);
+        initUI(rootView);
+
+        return rootView;
+    }
+
+    protected void initUI(View rootView) {
+        mList_nextPoints = (ListView) rootView.findViewById(R.id.list_learning_next_points);
+        mList_nextPoints.setOnItemClickListener(this);
+        mSwipe_nextPoints = (SwipeRefreshLayout) rootView.findViewById(R.id.swipe_next_points);
+        mSwipe_nextPoints.setOnRefreshListener(this);
+        mText_remainedTime = (TextView) rootView.findViewById(R.id.text_learning_remaining_time);
+        mImage_map = (ImageView) rootView.findViewById(R.id.image_learning_next_points);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // 裝置螢幕旋轉時，重新載入界面
+        // Get a layout inflater (inflater from getActivity() or getSupportActivity() works as well)
+        LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View newView = inflater.inflate(R.layout.fragment_study_guide, null);
+        // This just inflates the view but doesn't add it to any thing.
+        // You need to add it to the root view of the fragment
+        ViewGroup rootView = (ViewGroup) getView();
+        // Remove all the existing views from the root view.
+        // This is also a good place to recycle any resources you won't need anymore
+        rootView.removeAllViews();
+        rootView.addView(newView);
+
+        initUI(rootView);
+    }
+
+    /**
+     * Callback method to be invoked when an item in this AdapterView has
+     * been clicked.
+     * <p/>
+     * Implementers can call getItemAtPosition(position) if they need
+     * to access the data associated with the selected item.
+     *
+     * @param parent   The AdapterView where the click happened.
+     * @param view     The view within the AdapterView that was clicked (this
+     *                 will be a view provided by the adapter)
+     * @param position The position of the view in the adapter.
+     * @param id       The row id of the item that was clicked.
+     */
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onRefresh() {
+        mSwipe_nextPoints.setRefreshing(false);
+    }
+}
