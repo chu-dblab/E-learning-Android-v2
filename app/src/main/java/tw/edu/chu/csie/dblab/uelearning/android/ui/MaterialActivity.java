712c1c3791bd7a53ec0cc1ae663ab99e91c697f6
Yuan Chiu <chyuaner@gmail.com>
2015-01-16 17:31:42 +0800

    進入、離開學習點，對伺服端的通知

--- app/src/main/java/tw/edu/chu/csie/dblab/uelearning/android/ui/MaterialActivity.java
+++ app/src/main/java/tw/edu/chu/csie/dblab/uelearning/android/ui/MaterialActivity.java
@@ -12,8 +12,21 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.apache.http.Header;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import tw.edu.chu.csie.dblab.uelearning.android.R;
 import tw.edu.chu.csie.dblab.uelearning.android.config.Config;
+import tw.edu.chu.csie.dblab.uelearning.android.database.DBProvider;
+import tw.edu.chu.csie.dblab.uelearning.android.server.UElearningRestClient;
 import tw.edu.chu.csie.dblab.uelearning.android.util.ErrorUtils;
 import tw.edu.chu.csie.dblab.uelearning.android.util.FileUtils;
 
@@ -23,6 +36,11 @@ public class MaterialActivity extends ActionBarActivity {
      * 此學習點的標的編號
      */
     private int tId;
+
+    /**
+     * 是否為實際抵達學習點
+     */
+    private boolean isEntity;
 
     // UI上的元件
     private WebView mWebView;
@@ -37,6 +55,7 @@ public class MaterialActivity extends ActionBarActivity {
         // 取得目前所在的教材編號
         Intent intent = getIntent();
         this.tId = intent.getIntExtra("tId", 0);
+        this.isEntity = intent.getBooleanExtra("is_entity", true);
 
         // ActionBar對應
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_material);
@@ -81,9 +100,152 @@ public class MaterialActivity extends ActionBarActivity {
     }
 
     public void startLearn() {
+
+        final DBProvider db = new DBProvider(MaterialActivity.this);
+
+        // 抓取目前狀態所需資料
+        String token = db.get_token();
+        int saId = db.get_activity_id();
+
+        // 帶入參數
+        final RequestParams sId_params = new RequestParams();
+        if(isEntity) {
+            sId_params.put("is_entity", 1);
+        }
+        else {
+            sId_params.put("is_entity", 0);
+        }
+
+        // 告訴伺服端我已進入學習點
+        try {
+            final String url = "/tokens/" + URLEncoder.encode(token, HTTP.UTF_8) +
+                    "/activitys/" + saId + "/points/" + tId + "/toin";
+            UElearningRestClient.post(url, sId_params, new AsyncHttpResponseHandler() {
+
+                @Override
+                public void onStart() {
+
+                    if(Config.DEBUG_SHOW_MESSAGE) {
+                        Toast.makeText(MaterialActivity.this, "正在通知伺服端: "+url, Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    String content = null;
+
+                    try {
+                        content = new String(responseBody, "UTF-8");
+                        JSONObject response = new JSONObject(content);
+                        int sId = response.getInt("study_id");
+
+                        if(Config.DEBUG_SHOW_MESSAGE) {
+                            Toast.makeText(MaterialActivity.this, "已成功通知: "+sId, Toast.LENGTH_SHORT).show();
+                        }
+
+                    } catch (UnsupportedEncodingException e) {
+                        ErrorUtils.error(MaterialActivity.this, e);
+                    } catch (JSONException e) {
+                        ErrorUtils.error(MaterialActivity.this, e);
+                    }
+
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                    try {
+                        // TODO: 錯誤處理
+                        String content = new String(responseBody, HTTP.UTF_8);
+                        if(Config.DEBUG_SHOW_MESSAGE) {
+                            Toast.makeText(MaterialActivity.this,
+                                    "s: " + statusCode + "\n" + content,
+                                    Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(MaterialActivity.this, R.string.inside_error, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    catch (UnsupportedEncodingException e) {
+                        ErrorUtils.error(MaterialActivity.this, e);
+                    }
+                }
+            });
+        } catch (UnsupportedEncodingException e) {
+            ErrorUtils.error(MaterialActivity.this, e);
+        }
     }
 
     public void finishLearn() {
+
+        final DBProvider db = new DBProvider(MaterialActivity.this);
+
+        // 抓取目前狀態所需資料
+        String token = db.get_token();
+        int saId = db.get_activity_id();
+
+        // 帶入參數
+        final RequestParams out_params = new RequestParams();
+
+        // 告訴伺服端我已離開學習點
+        try {
+            final String url = "/tokens/" + URLEncoder.encode(token, HTTP.UTF_8) +
+                    "/activitys/" + saId + "/points/" + tId + "/toout";
+            UElearningRestClient.post(url, out_params, new AsyncHttpResponseHandler() {
+
+                @Override
+                public void onStart() {
+
+                    if(Config.DEBUG_SHOW_MESSAGE) {
+                        Toast.makeText(MaterialActivity.this, "正在通知伺服端: "+url, Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    String content = null;
+
+                    try {
+                        content = new String(responseBody, "UTF-8");
+                        JSONObject response = new JSONObject(content);
+
+                        if(Config.DEBUG_SHOW_MESSAGE) {
+                            Toast.makeText(MaterialActivity.this, "已成功通知: ", Toast.LENGTH_SHORT).show();
+                        }
+
+                    } catch (UnsupportedEncodingException e) {
+                        ErrorUtils.error(MaterialActivity.this, e);
+                    } catch (JSONException e) {
+                        ErrorUtils.error(MaterialActivity.this, e);
+                    }
+
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                    try {
+                        // TODO: 錯誤處理
+                        String content = new String(responseBody, HTTP.UTF_8);
+                        if(Config.DEBUG_SHOW_MESSAGE) {
+                            Toast.makeText(MaterialActivity.this,
+                                    "s: " + statusCode + "\n" + content,
+                                    Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(MaterialActivity.this, R.string.inside_error, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    catch (UnsupportedEncodingException e) {
+                        ErrorUtils.error(MaterialActivity.this, e);
+                    }
+                }
+            });
+        } catch (UnsupportedEncodingException e) {
+            ErrorUtils.error(MaterialActivity.this, e);
+        }
+
+        finish();
     }
 
     @Override
@@ -98,7 +260,7 @@ public class MaterialActivity extends ActionBarActivity {
         if(Config.LEARNING_BACK_ENABLE) {
             // 按兩下即離開學習點
             if (back_pressed + 2000 > System.currentTimeMillis()) {
-                finish();
+                finishLearn();
             }
             else {
                 Toast.makeText(getBaseContext(), R.string.double_back_press_to_exit_point, Toast.LENGTH_SHORT).show();
@@ -126,7 +288,7 @@ public class MaterialActivity extends ActionBarActivity {
 
             // 判斷目前的設定檔是否允許中途離開學習點
             if(Config.LEARNING_BACK_ENABLE) {
-                finish();
+                finishLearn();
             }
         }
 
