74a51d5ab711013ea990450457233927a1e6c39b
Yuan Chiu <chyuaner@gmail.com>
2015-04-01 16:09:31 +0800

    （未實作完成）上傳Log資料

--- app/src/main/java/tw/edu/chu/csie/dblab/uelearning/android/ui/LogActivity.java
+++ app/src/main/java/tw/edu/chu/csie/dblab/uelearning/android/ui/LogActivity.java
@@ -3,14 +3,24 @@ package tw.edu.chu.csie.dblab.uelearning.android.ui;
 import android.database.Cursor;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.ArrayMap;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import tw.edu.chu.csie.dblab.uelearning.android.R;
 import tw.edu.chu.csie.dblab.uelearning.android.database.DBProvider;
+import tw.edu.chu.csie.dblab.uelearning.android.util.ErrorUtils;
 
 public class LogActivity extends ActionBarActivity implements View.OnClickListener {
 
@@ -37,7 +47,50 @@ public class LogActivity extends ActionBarActivity implements View.OnClickListen
         Cursor query_allLog = db.getAll_log();
         int log_total = query_allLog.getCount();
 
-        mText_log_total.setText("Total"+log_total);
+        mText_log_total.setText("Total" + log_total);
+    }
+
+    public void uploadLogs() {
+        DBProvider db = new DBProvider(LogActivity.this);
+        Cursor query_allLog = db.getAll_log();
+        int log_total = query_allLog.getCount();
+
+        JSONArray logs = new JSONArray();
+        try {
+            for(int i=0; i<log_total; i++) {
+                query_allLog.moveToPosition(i);
+
+                String lId = query_allLog.getString(query_allLog.getColumnIndex("LID"));
+                String uId = query_allLog.getString(query_allLog.getColumnIndex("UID"));
+                String date = query_allLog.getString(query_allLog.getColumnIndex("Date"));
+                Integer saId = query_allLog.getInt(query_allLog.getColumnIndex("SaID"));
+                Integer tId = query_allLog.getInt(query_allLog.getColumnIndex("TID"));
+                String actionG = query_allLog.getString(query_allLog.getColumnIndex("ActionGroup"));
+                String action = query_allLog.getString(query_allLog.getColumnIndex("Encode"));
+                Integer qId = query_allLog.getInt(query_allLog.getColumnIndex("QID"));
+                String answer = query_allLog.getString(query_allLog.getColumnIndex("Aswer"));
+                String other = query_allLog.getString(query_allLog.getColumnIndex("Other"));
+
+                JSONObject thisLog = new JSONObject();
+                thisLog.put("LID", lId);
+                thisLog.put("UID", lId);
+                thisLog.put("Date", lId);
+                thisLog.put("SaID", lId);
+                thisLog.put("ActionGroup", lId);
+                thisLog.put("Encode", lId);
+                thisLog.put("TID", lId);
+                thisLog.put("QID", lId);
+                thisLog.put("Aswer", lId);
+                thisLog.put("Other", lId);
+
+                logs.put(thisLog);
+            }
+        } catch (JSONException e) {
+            ErrorUtils.error(LogActivity.this, e);
+        }
+
+        // 顯示挖出來的資料
+        Toast.makeText(this, logs.toString(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -66,7 +119,7 @@ public class LogActivity extends ActionBarActivity implements View.OnClickListen
     public void onClick(View v) {
         int id = v.getId();
         if(id == R.id.btn_log_upload_all) {
-
+            uploadLogs();
         }
     }
 }
